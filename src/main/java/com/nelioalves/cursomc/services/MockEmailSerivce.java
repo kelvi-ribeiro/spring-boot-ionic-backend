752a74e5fa46c9c898d6dd5fcd6b208accee15e6
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-03-08 03:19:09 -0300

    Aula 60. Enviando Email no html

--- src/main/java/com/nelioalves/cursomc/services/MockEmailSerivce.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailSerivce.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -14,5 +16,14 @@ public class MockEmailSerivce extends AbstractEmailService {
 		LOG.info("Email Enviado");
 
 	}
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando Envio de email HTML. . .");
+		LOG.info(msg.toString());
+		LOG.info("Email Enviado");
+		
+	}
+	
+	
 
 }
