55fa0d493beec8a36b9a695c003a9781eb9fca01
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-03-08 01:12:24 -0300

    Aula 58. MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/nelioalves/cursomc/services/PedidoService.java
+++ src/main/java/com/nelioalves/cursomc/services/PedidoService.java
@@ -37,6 +37,9 @@ public class PedidoService {
 	@Autowired	
 	private ClienteRepository clienteRepository;
 	
+	@Autowired
+	private EmailService emailService;
+	
 	public Pedido find(Integer id) {
 		Pedido obj = repo.findOne(id);
 		if (obj == null) {
@@ -66,7 +69,7 @@ public class PedidoService {
 		}
 		
 		itemPedidoRepository.save(obj.getItens());
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 	
