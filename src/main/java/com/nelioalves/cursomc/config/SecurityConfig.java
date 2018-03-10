0f17c4d20048d25cc0562ae02f84dc3aa17128aa
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-03-10 16:12:10 -0300

    Aula 64. Salvado perfis de usuario na base de dados

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -19,45 +19,44 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
-	private Environment env;
+    private Environment env;
+	
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**"
-			
-			
 	};
-	
-	private static final String[] PUBLIC_MATCHERS_GET = {			
-			"/produtos/**",
-			"/categorias/**"
-	};
-	@Override
-	protected void configure (HttpSecurity http) throws Exception{
 
-			if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/produtos/**",
+			"/categorias/**",
+			"/clientes/**"
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
+		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-		.anyRequest().authenticated();
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
-			final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-			return source;
-		}
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
+	}
 	
 	@Bean
-	public BCryptPasswordEncoder bcryptPasswordEncoder() {
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
 }
-
-
