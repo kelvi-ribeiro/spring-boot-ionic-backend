838bb7747cd1b2dab771d89db2375b63fc97aff8
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-03-24 08:07:19 -0300

    Corrigindo problema de Cors

--- src/main/java/com/nelioalves/cursomc/resources/AuthResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/AuthResource.java
@@ -31,7 +31,7 @@ public class AuthResource {
 		UserSS user = UserService.authenticated();
 		String token = jwtUtil.generateToken(user.getUsername());
 		response.addHeader("Authorization", "Bearer " + token);
-		response.addHeader("acess-control-expose-headers","Authorization");
+		response.addHeader("access-control-expose-headers", "Authorization");
 		return ResponseEntity.noContent().build();
 	}
 
